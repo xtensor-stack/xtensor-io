4c77eda1828aa1924050178ecab2aeb1be60aaf1
Andrew Hardin <andrew.hardin5+git@gmail.com>
2020-08-25 22:19:42 -0600

    Add unit tests for writing.

--- test/test_xgdal.cpp
+++ test/test_xgdal.cpp
@@ -25,15 +25,22 @@ namespace xt
             GDALRegister_GTiff();
             auto driver = GDALGetDriverByName("GTiff");
             ASSERT_NE(driver, nullptr);
-            auto ds = GDALCreate(driver, path.c_str(), nx, ny, bands, GDT_Int32, nullptr);
+            auto ds = GDALCreate(driver, in_path.c_str(), nx, ny, bands, GDT_Int32, nullptr);
             ASSERT_NE(ds, nullptr);
             auto err = GDALDatasetRasterIO(ds, GDALRWFlag::GF_Write, 0, 0, nx, ny, data.data(), nx, ny,
                                            GDALDataType::GDT_Int32, bands, nullptr, 0, 0, 0);
             ASSERT_EQ(err, CPLErr::CE_None);
             GDALClose(ds);
         }
+        void TearDown() override
+        {
+            // Delete the in-memory datasets.
+            VSIUnlink(in_path.c_str());
+            VSIUnlink(out_path.c_str());
+        }
 
-        std::string path = "/vsimem/test_file.tif";
+        std::string in_path = "/vsimem/test_file.tif";
+        std::string out_path = "/vsimem/test_file_output.tif";
         int bands = 2;
         int nx = 4;
         int ny = 3;
@@ -55,16 +62,102 @@ namespace xt
 
     };
 
-    TEST_F(xgdal_fixture, interleave)
+    TEST_F(xgdal_fixture, load_invalid_interleave)
     {
-        // Check the six interleave options.
+        load_gdal_options options;
+        options.interleave = {component::band, component::column, component::column};
+        EXPECT_THROW(load_gdal<int>(in_path, options), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, load_file_missing)
+    {
+        EXPECT_THROW(load_gdal<int>(in_path + "/file-dne.tif", {}), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, load_silent_downcast)
+    {
+        // Verify that GDAL silently downcasts the type from int to byte.
+        auto test = load_gdal<uint8_t>(in_path, {});
+        auto truth = xt::adapt(data, test.shape());
+        EXPECT_TRUE(all(equal(xt::cast<uint8_t>(truth), test)));
+    }
+
+    TEST_F(xgdal_fixture, load_invalid_band_args)
+    {
+        load_gdal_options opt;
+        opt.bands_to_load = {0}; // band out-of-range.
+        EXPECT_THROW(load_gdal<int>(in_path, opt), std::runtime_error);
+
+        opt.bands_to_load = {3}; // band out-of-range.
+        EXPECT_THROW(load_gdal<int>(in_path, opt), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, load_selected_band)
+    {
+        load_gdal_options opt;
+        opt.bands_to_load = {2};
+        auto test = load_gdal<int>(in_path, opt);
+        auto truth = xt::adapt(data.data() + nx * ny, test.shape());
+        EXPECT_TRUE(all(equal(truth, test)));
+    }
+
+    TEST_F(xgdal_fixture, load_custom_error_handler)
+    {
+        bool thrown = false;
+        load_gdal_options opt;
+        opt.error_handler = [&](const std::string &)
+        { thrown = true; };
+        auto test = load_gdal<int>(in_path + "/file-dne.tif", opt);
+        EXPECT_TRUE(thrown);
+        EXPECT_EQ(test.size(), 0);
+    }
+
+    TEST_F(xgdal_fixture, dump_invalid_interleave)
+    {
+        dump_gdal_options options;
+        options.interleave = {component::band, component::column, component::column};
+        EXPECT_THROW(dump_gdal(load_gdal<int>(in_path, {}), out_path, options), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, dump_gdal_missing_driver)
+    {
+        dump_gdal_options options;
+        options.driver_name = "missing_driver";
+        EXPECT_THROW(dump_gdal(load_gdal<int>(in_path, {}), out_path, options), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, dump_invalid_shape)
+    {
+        // Either too few or too many dimensions.
+        dump_gdal_options options;
+        xt::xtensor<int, 0> nil;
+        EXPECT_THROW(dump_gdal(nil, out_path, {}), std::runtime_error);
+        xt::xtensor<int, 4> d4;
+        EXPECT_THROW(dump_gdal(d4, out_path, {}), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, dump_invalid_path)
+    {
+        dump_gdal_options options;
+        EXPECT_THROW(dump_gdal(load_gdal<int>(in_path, {}), "/path/to/neverland", options), std::runtime_error);
+    }
+
+    TEST_F(xgdal_fixture, load_dump_interleave_roundtrip)
+    {
+        // Check the six interleave options passed through dump() and load().
         auto check = [&](component a, component b, component c, const std::vector<int> &vtruth)
         {
-            load_gdal_options options;
-            options.interleave = {a, b, c};
-            auto test = load_gdal<int>(path, options);
+            load_gdal_options lopt;
+            lopt.interleave = {a, b, c};
+            auto test = load_gdal<int>(in_path, lopt);
             auto truth = xt::adapt(vtruth, test.shape());
-            EXPECT_TRUE(all(equal(truth, test)));
+            EXPECT_TRUE(all(equal(truth, test))) << "load failed";
+
+            dump_gdal_options dopt;
+            dopt.interleave = lopt.interleave;
+            EXPECT_EQ(dump_gdal(truth, out_path, dopt), nullptr);
+            test = load_gdal<int>(out_path, lopt);
+            EXPECT_TRUE(all(equal(truth, test))) << "dump failed";
         };
         check(component::band, component::row, component::column,
               data);
@@ -80,53 +173,37 @@ namespace xt
               {1, 13, 5, 17, 9, 21, 2, 14, 6, 18, 10, 22, 3, 15, 7, 19, 11, 23, 4, 16, 8, 20, 12, 24});
     }
 
-    TEST_F(xgdal_fixture, invalid_interleave)
+    TEST_F(xgdal_fixture, dump_supports_2d)
     {
-        load_gdal_options options;
-        options.interleave = {component::band, component::column, component::column};
-        EXPECT_THROW(load_gdal<int>(path, options), std::runtime_error);
+        // Check that the 2D convenience hook is wired up.
+        xtensor<int, 2> simple = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8}
+        };
+        dump_gdal(simple, out_path, {});
+        auto test = load_gdal<int>(out_path, {});
+        EXPECT_TRUE(all(equal(test, simple)));
     }
 
-    TEST_F(xgdal_fixture, file_missing)
+    TEST_F(xgdal_fixture, fancy_expression)
     {
-        EXPECT_THROW(load_gdal<int>(path + "/file-dne.tif", {}), std::runtime_error);
+        // Sanity check that fancy expressions are handled.
+        xtensor<double, 2> values = {{ 2. }};
+        dump_gdal(sqrt(values), out_path, {});
+        auto test = load_gdal<double>(out_path, {});
+        EXPECT_DOUBLE_EQ(test[0], std::sqrt(2.));
     }
 
-    TEST_F(xgdal_fixture, silent_downcast)
+    TEST_F(xgdal_fixture, dump_leaves_dataset_open)
     {
-        // Verify that GDAL silently downcasts the type from int to byte.
-        auto test = load_gdal<uint8_t>(path, {});
-        auto truth = xt::adapt(data, test.shape());
-        EXPECT_TRUE(all(equal(xt::cast<uint8_t>(truth), test)));
-    }
-
-    TEST_F(xgdal_fixture, invalid_band_args)
-    {
-        load_gdal_options opt;
-        opt.bands_to_load = {0}; // band out-of-range.
-        EXPECT_THROW(load_gdal<int>(path, opt), std::runtime_error);
-
-        opt.bands_to_load = {3}; // band out-of-range.
-        EXPECT_THROW(load_gdal<int>(path, opt), std::runtime_error);
-    }
-
-    TEST_F(xgdal_fixture, selected_band)
-    {
-        load_gdal_options opt;
-        opt.bands_to_load = {2};
-        auto test = load_gdal<int>(path, opt);
-        auto truth = xt::adapt(data.data() + nx * ny, test.shape());
-        EXPECT_TRUE(all(equal(truth, test)));
-    }
-
-    TEST_F(xgdal_fixture, custom_error_handler)
-    {
-        bool thrown = false;
-        load_gdal_options opt;
-        opt.error_handler = [&](const std::string &)
-        { thrown = true; };
-        auto test = load_gdal<int>(path + "/file-dne.tif", opt);
-        EXPECT_TRUE(thrown);
-        EXPECT_EQ(test.size(), 0);
+        dump_gdal_options opt;
+        opt.return_opened_dataset = true;
+        opt.creation_options.emplace_back("COMPRESS=DEFLATE");  // also try out an create_option.
+        auto dataset = dump_gdal(load_gdal<int>(in_path, {}), out_path, opt);
+        EXPECT_NE(dataset, nullptr);
+        EXPECT_EQ(GDALGetRasterCount(dataset), bands);
+        EXPECT_EQ(GDALGetRasterXSize(dataset), nx);
+        EXPECT_EQ(GDALGetRasterYSize(dataset), ny);
+        GDALClose(dataset);
     }
 }  // namespace xt
