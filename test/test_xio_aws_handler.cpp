e4d52fef225acda5fcd74f3db57b6b1187170c33
David Brochart <david.brochart@gmail.com>
2020-11-17 00:54:59 +0100

    Add AWS S3 IO handler

---
+++ test/test_xio_aws_handler.cpp
@@ -0,0 +1,39 @@
+/***************************************************************************
+* Copyright (c) Wolf Vollprecht, Sylvain Corlay and Johan Mabille          *
+* Copyright (c) QuantStack                                                 *
+*                                                                          *
+* Distributed under the terms of the BSD 3-Clause License.                 *
+*                                                                          *
+* The full license is in the file LICENSE, distributed with this software. *
+****************************************************************************/
+
+#include <cstdint>
+#include <sstream>
+#include <exception>
+
+#include "gtest/gtest.h"
+#include "xtensor/xview.hpp"
+#include "xtensor-io/xio_binary.hpp"
+#include "xtensor-io/xio_aws_handler.hpp"
+
+namespace xt
+{
+    TEST(xio_aws_handler, read)
+    {
+        Aws::SDKOptions options;
+        Aws::InitAPI(options);
+
+        xio_aws_handler<xio_binary_config> h;
+
+        Aws::S3::S3Client client;
+
+        xio_aws_config c = {client, "elevation-tiles-prod"};
+        h.configure_io(c);
+        xarray<char> a0;
+        h.read(a0, "main.js");
+        xarray<char> a1 = {'/', '*', 'j', 's', 'l', 'i', 'n', 't', ' ', 'b', 'r' ,'o', 'w', 's', 'e', 'r', ':', ' ', 't', 'r', 'u', 'e', '*', '/'};
+        EXPECT_TRUE(xt::all(xt::equal(xt::view(a0, xt::range(0, 24)), a1)));
+
+        Aws::ShutdownAPI(options);
+    }
+}
